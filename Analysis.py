7709afb012f81e661bdc3a4b7b0afe084e4639c6
rajan <rajanbhateja6@gmail.com>
2024-10-08 22:36:29 +0530

    Created Analysis.py

---
+++ Analysis.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import Welcome
+
+# for some reason, it's looping
+# I can't figure out why.
+# If you guys manage to figure out, update me and the code
+
+#def analyze_csv(file_path):
+#    try:
+#        df = pd.read_csv(file_path)
+#        print(df)
+
+
+#    except Exception as e:
+#        print(f"Error occurred while processing the file: {e}")
