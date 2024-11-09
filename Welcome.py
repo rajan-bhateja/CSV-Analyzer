9e5e8dd0218c2b4fd94f37766457f66e95d589bc
rajan <rajanbhateja6@gmail.com>
2024-11-09 21:40:30 +0530

    Font change

--- Welcome.py
+++ Welcome.py
@@ -22,18 +22,18 @@ welcome.config(background='#FFFFFF')                # Setting the background col
 # Label stuff
 logo = PhotoImage(file="Logo no_background 147_200.png")
 icon = logo
-label = Label(welcome, text="CSV Analyzer", bg='#FFFFFF', fg='#000000', font=('Arial', 40, 'bold'),
+label = Label(welcome, text="CSV Analyzer", bg='#FFFFFF', fg='#000000', font=('Helvetica', 40, 'bold'),
               padx=20, pady=20, image=icon, compound='top')
 label.pack()
 
 # intro to CSV Analyzer
 text = Label(text="A simple application to\n"
                   "display some statistics.",
-             bg='#FFFFFF', fg='#000000', font=('Arial', 16), padx=20, pady=20)
+             bg='#FFFFFF', fg='#000000', font=('Helvetica', 16), padx=20, pady=20)
 text.pack()                                         # display the text
 
 # click to analyze button
-open_btn = Button(welcome, text="Select CSV file", font=("Arial", 12),
+open_btn = Button(welcome, text="Select CSV file", font=("Helvetica", 12),
              border=2, bg='#FFFFFF', fg='#000000', padx=10, compound='bottom',
              command=open_file)
 open_btn.pack()                                     # display the button
